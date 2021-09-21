2dc0e8104a7610c2ca0fcff5748f827b536b9c45
independencia <ind@ip-172-31-5-85.sa-east-1.compute.internal>
2021-09-21 23:24:46 +0000

    Configuracion EC2

--- datalake_github/datalake/datalake/settings.py
+++ datalake_github/datalake/datalake/settings.py
@@ -88,13 +88,23 @@ WSGI_APPLICATION = 'datalake.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': str(BASE_DIR / 'datalake_db'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'datalake_db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'datalake_db',
+        'USER': 'datalake_master',
+        'PASSWORD': 'D4t4l4k3db.',
+        'HOST': 'datalake-db.cplck86bddfa.sa-east-1.rds.amazonaws.com',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
