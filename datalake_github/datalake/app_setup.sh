d5b0116f82631e3ae2a0461f6feee17d7dc4334d
tomaschild <tomas.child@usach.cl>
2021-12-14 23:47:43 -0300

    se agrega script de configuración inicial

---
+++ datalake_github/datalake/app_setup.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py loaddata core/fixtures/core/CI.json
+python manage.py loaddata core/fixtures/core/UV.json
+python manage.py loaddata core/fixtures/core/P0.json
+python manage.py loaddata core/fixtures/core/T0.json
+python manage.py loaddata core/fixtures/core/C0.json
+python manage.py loaddata core/fixtures/core/D0.json
