0801d40f5b649b9a4e9a15b8af2126d9d29e3918
Chacon <63523896+Chacon-tu-drema@users.noreply.github.com>
2021-12-21 12:38:08 -0300

    Limpieza de archivos
    Se limpia develop para unirlo con producción

--- datalake_github/datalake/vis/admin.py
+++ datalake_github/datalake/vis/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import (
-    VisFarmacia,
-)
 
-admin.site.register(VisFarmacia)
+
+
--- datalake_github/datalake/vis/models.py
+++ datalake_github/datalake/vis/models.py
@@ -1,7 +1,2 @@
 from django.db import models
 
-class VisFarmacia(models.Model):
-    farmacia_archivo = models.FileField(upload_to='vis/data/farmacia/', verbose_name="Archivo Farmacia")
-
-    def __str__(self):
-        return f'{self.id}'
