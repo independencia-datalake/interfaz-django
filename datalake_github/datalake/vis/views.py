dc5c87b5b89c35649064b8a99e32eb9892583ec0
Chacon <63523896+Chacon-tu-drema@users.noreply.github.com>
2022-02-07 11:39:13 +0000

    Actualizacion Calles independencia
    Se incluyeron las ultimas calles que faltaban en CSV de streets_uv y agregaron al fixtures que incluye las calles a la base de datos CI.json.

--- datalake_github/datalake/vis/views.py
+++ datalake_github/datalake/vis/views.py
@@ -17,10 +17,6 @@ from carga.models import(
     )
 from django.contrib.auth.decorators import login_required
 
-# #CONFIGURACION DEL S3 AWS
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 #FARMACIA
 @login_required
