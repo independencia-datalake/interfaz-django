8028222406ebe4e2f6f000957eb67e018675f133
danoloparedes <67171719+danoloparedes@users.noreply.github.com>
2022-12-19 17:20:23 -0300

    Revert "Merge pull request #25 from independencia-datalake/develop"
    This reverts commit 25c29663bd3ab9553ecb737d77d46be49063ac01, reversing
    changes made to ab1dcd6aa1e3f63e58b1f29289acb0999f61a251.

--- datalake_github/datalake/vis/views.py
+++ datalake_github/datalake/vis/views.py
@@ -20,8 +20,7 @@ from carga.models import(
     ExencionAseo,
     DOM
 )
-import locale
-locale.setlocale(locale.LC_ALL,'es_ES.UTF-8')
+
 
 def inicio_vis(request):
     filtro_tiempo=FiltroTiempo(request.POST or None)
@@ -86,8 +85,11 @@ def farmacia_vis(request):
             tiempo = {"max":c.max,"min": c.min}
 
 
-        fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S.%f')
-        fecha_fin = datetime.strptime(tiempo['max'], '%Y-%m-%d %H:%M:%S.%f')
+        #fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S.%f')
+        #fecha_fin = datetime.strptime(tiempo['max'], '%Y-%m-%d %H:%M:%S.%f')
+
+        fecha_inicio = datetime.strftime(tiempo['min'], '%Y-%m-%d')
+        fecha_fin = datetime.strftime(tiempo['max'], '%Y-%m-%d')
 
         fechas_categoria = {
             'fecha_inicio': fecha_inicio,
@@ -322,11 +324,12 @@ def dimap_vis(request,categoria):
                             GROUP BY a.created, a.uc
                             order by a.created;'''
 
-        for c in SeguridadDIMAP.objects.raw(query_tiempo): #!todo OJO ACA // MODEL EXTRAÑO
+        for c in ComprobanteVenta.objects.raw(query_tiempo): #!todo OJO ACA // MODEL EXTRAÑO
             tiempo = {"max":c.max,"min": c.min}
 
-        fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S.%f')
-        fecha_fin = datetime.strptime(tiempo['max'], '%Y-%m-%d %H:%M:%S.%f')
+        fecha_inicio = datetime.strftime(tiempo['min'], '%Y-%m-%d')
+        fecha_fin = datetime.strftime(tiempo['max'], '%Y-%m-%d')
+
         
 
         fechas_categoria = {
@@ -729,7 +732,7 @@ def seguridad_vis(request, categoria):
                             where sr.uv_id <> 0
                             order by sr.created asc'''
 
-        for c in Requerimiento.objects.raw(query_tiempo): #!todo OJO ACA // MODEL EXTRAÑO
+        for c in ComprobanteVenta.objects.raw(query_tiempo): #!todo OJO ACA // MODEL EXTRAÑO
             tiempo = {"max":c.max,"min": c.min}
 
         fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S.%f')
@@ -1110,7 +1113,7 @@ def exencion_vis(request, categoria):
                             where ce.uv_id <> 0
                             order by ce.marca_temporal asc;'''
 
-        for c in ExencionAseo.objects.raw(query_tiempo):
+        for c in ComprobanteVenta.objects.raw(query_tiempo):
             tiempo = {"max":c.max,"min": c.min}
 
         fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S')
@@ -1474,7 +1477,7 @@ def entrega_pandemia_vis(request, categoria):
                             where ce.uv_id <> 1
                             order by ce.fecha asc;'''
 
-        for c in EntregasPandemia.objects.raw(query_tiempo):
+        for c in ComprobanteVenta.objects.raw(query_tiempo):
             tiempo = {"max":c.max,"min": c.min}
 
         fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d')
@@ -1920,7 +1923,7 @@ def impuestos_derechos_vis(request,categoria):
                             where ce.uv_id <> 0
                             order by ce.created asc;'''
 
-        for c in Empresas.objects.raw(query_tiempo):
+        for c in ComprobanteVenta.objects.raw(query_tiempo):
             tiempo = {"max":c.max,"min": c.min}
 
         fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S.%f')
@@ -2237,7 +2240,7 @@ def transito_vis(request, categoria):
                           ORDER BY fecha'''
 
 
-        for c in LicenciaConducir.objects.raw(query_tiempo):
+        for c in ComprobanteVenta.objects.raw(query_tiempo):
             tiempo = {"max":c.max,"min": c.min}
 
         fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d')
@@ -2718,7 +2721,7 @@ def obras_municipales_vis(request,categoria):
                             order by cd.created asc;'''
 
 
-        for c in DOM.objects.raw(query_tiempo):
+        for c in ComprobanteVenta.objects.raw(query_tiempo):
             tiempo = {"max":c.max,"min": c.min}
 
         fecha_inicio = datetime.strptime(tiempo['min'], '%Y-%m-%d %H:%M:%S.%f')
