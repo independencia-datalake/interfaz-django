a193c975ceee9f7515faa2e653c6b6cf2dd4db15
danoloparedes <67171719+danoloparedes@users.noreply.github.com>
2023-01-09 14:17:54 -0300

    filtro seguridad
    se saco el init de filtro seguridad

--- datalake_github/datalake/seguridad/filters.py
+++ datalake_github/datalake/seguridad/filters.py
@@ -23,17 +23,3 @@ class RequerimientoFilter(django_filters.FilterSet):
             'estatus',
         }
 
-    def __init__(self, data=None, *args, **kwargs):
-        # if filterset is bound, use initial values as defaults
-        if data is not None:
-            # get a mutable copy of the QueryDict
-            data = data.copy()
-
-            for name, f in self.base_filters.items():
-                initial = f.extra.get('choices')
-
-                # filter param is either missing or empty, use initial as default
-                if not data.get(name) and name == 'estatus':
-                    data[name] = initial[2][0]
-
-        super().__init__(data, *args, **kwargs)
